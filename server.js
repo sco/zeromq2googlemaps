0b8183a60730b37832971aa9c8ef454bffc111d7
Scott Raymond <sco@scottraymond.net>
2011-06-19 00:40:46 -0500

    nothing

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var http        = require('http'),
     pusher      = require('./pusher');
 
 // create a zeromq 'sub' socket and subscribe to all messages
-// var subscriber = zeromq.createSocket('sub')
-// subscriber.subscribe('')
+var subscriber = zeromq.createSocket('sub')
+subscriber.subscribe('')
 
 // configuration from pusher.com
 var pusherConfig = {
@@ -15,25 +15,25 @@ var pusherConfig = {
   secret: 'a5812e6ebc7de67b16af'
 };
 
-// register callback for when a zeromq message is received
-// subscriber.on('message', function(data) {
-// 	// parse the message
-// 	var message = querystring.parse(data.toString());
-// 
-// 	// clean it up a bit and send it to pusher
-// 	pusher.trigger(pusherConfig, "test_channel", "my_event", {
-// 		lat:       parseFloat(message.lat),
-// 		lng:       parseFloat(message.lon),
-// 		yield:     parseFloat(message.yield),
-// 		combineId: message['combine-id'],
-// 		timestamp: Date.parse(message.time),
-// 		state:     message.state,
-// 		county:    message.county
-// 	});
-// })
+register callback for when a zeromq message is received
+subscriber.on('message', function(data) {
+	// parse the message
+	var message = querystring.parse(data.toString());
+
+	// clean it up a bit and send it to pusher
+	pusher.trigger(pusherConfig, "test_channel", "my_event", {
+		lat:       parseFloat(message.lat),
+		lng:       parseFloat(message.lon),
+		yield:     parseFloat(message.yield),
+		combineId: message['combine-id'],
+		timestamp: Date.parse(message.time),
+		state:     message.state,
+		county:    message.county
+	});
+})
 
 // connect to the remote pub socket
-//subscriber.connect("tcp://66.206.206.12:61991")
+subscriber.connect("tcp://66.206.206.12:61991")
 
 // create an http server for the HTML file
 http.createServer(function (req, res) {
