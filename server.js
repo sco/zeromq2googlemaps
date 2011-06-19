fd64d488569509279a266ff7c8cce11d580933ca
Scott Raymond <sco@scottraymond.net>
2011-06-19 00:15:33 -0500

    nothing

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
-// var http        = require('http'),
-//     fs          = require('fs'),
-//     zeromq      = require('zeromq'),
-//     querystring = require('querystring'),
-//     pusher      = require('./pusher');
-// 
-// // create a zeromq 'sub' socket and subscribe to all messages
-// var subscriber = zeromq.createSocket('sub')
-// subscriber.subscribe('')
-// 
-// // configuration from pusher.com
-// var pusherConfig = {
-//   appId:  '173',
-//   key:    '09b153199c3ffc4e1e41',
-//   secret: 'a5812e6ebc7de67b16af'
-// };
-// 
-// // register callback for when a zeromq message is received
-// subscriber.on('message', function(data) {
-// 	// parse the message
-// 	var message = querystring.parse(data.toString());
-// 
-// 	// clean it up a bit and send it to pusher
-// 	pusher.trigger(pusherConfig, "test_channel", "my_event", {
-// 		lat:       parseFloat(message.lat),
-// 		lng:       parseFloat(message.lon),
-// 		yield:     parseFloat(message.yield),
-// 		combineId: message['combine-id'],
-// 		timestamp: Date.parse(message.time),
-// 		state:     message.state,
-// 		county:    message.county
-// 	});
-// })
-// 
-// // connect to the remote pub socket
-// subscriber.connect("tcp://66.206.206.12:61991")
-
 var http        = require('http'),
-    fs          = require('fs');
+    fs          = require('fs'),
+    zeromq      = require('zeromq'),
+    querystring = require('querystring'),
+    pusher      = require('./pusher');
+
+// create a zeromq 'sub' socket and subscribe to all messages
+var subscriber = zeromq.createSocket('sub')
+subscriber.subscribe('')
+
+// configuration from pusher.com
+var pusherConfig = {
+  appId:  '173',
+  key:    '09b153199c3ffc4e1e41',
+  secret: 'a5812e6ebc7de67b16af'
+};
+
+// register callback for when a zeromq message is received
+subscriber.on('message', function(data) {
+	// parse the message
+	var message = querystring.parse(data.toString());
+
+	// clean it up a bit and send it to pusher
+	pusher.trigger(pusherConfig, "test_channel", "my_event", {
+		lat:       parseFloat(message.lat),
+		lng:       parseFloat(message.lon),
+		yield:     parseFloat(message.yield),
+		combineId: message['combine-id'],
+		timestamp: Date.parse(message.time),
+		state:     message.state,
+		county:    message.county
+	});
+})
+
+// connect to the remote pub socket
+subscriber.connect("tcp://66.206.206.12:61991")
 
 // create an http server for the HTML file
 http.createServer(function (req, res) {
